f5c603324b36a5ae8301f14bfec7ce8a84918c74
Andrew C <achow101@gmail.com>
2015-12-23 16:35:54 -0500

    Reduce estimates
    Reduced the estimates to better match current prices

--- src/com/achow101/bctalkaccountpricer/server/AccountPricer.java
+++ src/com/achow101/bctalkaccountpricer/server/AccountPricer.java
@@ -398,11 +398,11 @@ public class AccountPricer {
 	
 	private double estimatePrice(int activity, int potentialActivity, double ratio, int trust)
 	{
-		double price = 0.0006 * activity;
+		double price = 0.0004 * activity;
 		
 		// Extra potential activity
 		int epa = potentialActivity - activity;
-		price += epa * 0.0003;
+		price += epa * 0.0002;
 		
 		// Post quality multipliers
 		
